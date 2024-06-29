226097a8e0d10453aecf75900aa775a641a18ebd
0vinayak <160499383+0vinayak@users.noreply.github.com>
2024-06-29 16:22:10 +0530

    SomResetResolved

--- components/ql-application/peripheral/gpio_demo.c
+++ components/ql-application/peripheral/gpio_demo.c
@@ -58,9 +58,9 @@ static void ql_gpio_demo_thread(void *param)
 {
     QL_GPIODEMO_LOG("gpio demo thread enter, param 0x%x", param);
 
-    // ql_event_t event;
+    ql_event_t event;
 
-    // uint8_t change_flg = 0;
+    uint8_t change_flg = 0;
     uint16_t num;
 
     ql_GpioDir  gpio_dir;
@@ -91,122 +91,122 @@ static void ql_gpio_demo_thread(void *param)
     }
     ql_rtos_task_sleep_s(3);
 
-    // while(1)
-    // {
-    //     ql_event_wait(&event, 1);
+    while(1)
+    {
+        ql_event_wait(&event, 1);
 
-    //     /* output low test */
-    //     gpio_dir  = GPIO_OUTPUT;
-    //     gpio_lvl  = LVL_LOW;
-    //     for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
-    //     {
-    //         /* set output low */
-    //         ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
-    //         ql_gpio_set_level(_ql_gpio_cfg[num].gpio_num, gpio_lvl);
+        /* output low test */
+        gpio_dir  = GPIO_OUTPUT;
+        gpio_lvl  = LVL_LOW;
+        for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
+        {
+            /* set output low */
+            ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
+            ql_gpio_set_level(_ql_gpio_cfg[num].gpio_num, gpio_lvl);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] output low-level", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
+            QL_GPIODEMO_LOG("gpio[%d] output low-level", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
 
-    //         /* get output low */
-    //         ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
-    //         ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
+            /* get output low */
+            ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
+            ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] output low-level", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
-    //     }
-    //     ql_rtos_task_sleep_s(3);
+            QL_GPIODEMO_LOG("gpio[%d] output low-level", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
+        }
+        ql_rtos_task_sleep_s(3);
 
-    //     /* output high test */
-    //     gpio_dir  = GPIO_OUTPUT;
-    //     gpio_lvl  = LVL_HIGH;
-    //     for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
-    //     {
-    //         /* set output high */
-    //         ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
-    //         ql_gpio_set_level(_ql_gpio_cfg[num].gpio_num, gpio_lvl);
+        /* output high test */
+        gpio_dir  = GPIO_OUTPUT;
+        gpio_lvl  = LVL_HIGH;
+        for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
+        {
+            /* set output high */
+            ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
+            ql_gpio_set_level(_ql_gpio_cfg[num].gpio_num, gpio_lvl);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] output high-level", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
+            QL_GPIODEMO_LOG("gpio[%d] output high-level", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
 
-    //         /* get output high */
-    //         ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
-    //         ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
+            /* get output high */
+            ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
+            ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] output high-level", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
-    //     }
-    //     ql_rtos_task_sleep_s(3);
+            QL_GPIODEMO_LOG("gpio[%d] output high-level", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_lvl);
+        }
+        ql_rtos_task_sleep_s(3);
 
-    //     /* input pull-down test */
-    //     gpio_dir  = GPIO_INPUT;
-    //     gpio_pull = PULL_DOWN;
-    //     for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
-    //     {
-    //         /* set input pull-down */
-    //         ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
-    //         ql_gpio_set_pull(_ql_gpio_cfg[num].gpio_num, gpio_pull);
+        /* input pull-down test */
+        gpio_dir  = GPIO_INPUT;
+        gpio_pull = PULL_DOWN;
+        for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
+        {
+            /* set input pull-down */
+            ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
+            ql_gpio_set_pull(_ql_gpio_cfg[num].gpio_num, gpio_pull);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] input pull-down", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], pull:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull);
+            QL_GPIODEMO_LOG("gpio[%d] input pull-down", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], pull:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull);
 
-    //         /* get input pull-down */
-    //         ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
-    //         ql_gpio_get_pull(_ql_gpio_cfg[num].gpio_num, &gpio_pull);
-    //         ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
+            /* get input pull-down */
+            ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
+            ql_gpio_get_pull(_ql_gpio_cfg[num].gpio_num, &gpio_pull);
+            ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] input pull-down", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], pull:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull, gpio_lvl);
-    //     }
-    //     ql_rtos_task_sleep_s(3);
+            QL_GPIODEMO_LOG("gpio[%d] input pull-down", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], pull:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull, gpio_lvl);
+        }
+        ql_rtos_task_sleep_s(3);
 
-    //     /* input pull-up test */
-    //     gpio_dir  = GPIO_INPUT;
-    //     gpio_pull = PULL_UP;
-    //     for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
-    //     {
-    //         /* set input pull-up */
-    //         ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
-    //         ql_gpio_set_pull(_ql_gpio_cfg[num].gpio_num, gpio_pull);
+        /* input pull-up test */
+        gpio_dir  = GPIO_INPUT;
+        gpio_pull = PULL_UP;
+        for( num = 0; num < sizeof(_ql_gpio_cfg)/sizeof(_ql_gpio_cfg[0]); num++ )
+        {
+            /* set input pull-up */
+            ql_gpio_set_direction(_ql_gpio_cfg[num].gpio_num, gpio_dir);
+            ql_gpio_set_pull(_ql_gpio_cfg[num].gpio_num, gpio_pull);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] input pull-up", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], pull:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull);
+            QL_GPIODEMO_LOG("gpio[%d] input pull-up", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] set dir:[%d], pull:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull);
 
-    //         /* get input pull-up */
-    //         ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
-    //         ql_gpio_get_pull(_ql_gpio_cfg[num].gpio_num, &gpio_pull);
-    //         ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
+            /* get input pull-up */
+            ql_gpio_get_direction(_ql_gpio_cfg[num].gpio_num, &gpio_dir);
+            ql_gpio_get_pull(_ql_gpio_cfg[num].gpio_num, &gpio_pull);
+            ql_gpio_get_level(_ql_gpio_cfg[num].gpio_num, &gpio_lvl);
 
-    //         QL_GPIODEMO_LOG("gpio[%d] input pull-up", _ql_gpio_cfg[num].gpio_num);
-    //         QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], pull:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull, gpio_lvl);
-    //     }
-    //     ql_rtos_task_sleep_s(3);
+            QL_GPIODEMO_LOG("gpio[%d] input pull-up", _ql_gpio_cfg[num].gpio_num);
+            QL_GPIODEMO_LOG("gpio[%d] get dir:[%d], pull:[%d], lvl:[%d]", _ql_gpio_cfg[num].gpio_num, gpio_dir, gpio_pull, gpio_lvl);
+        }
+        ql_rtos_task_sleep_s(3);
 
-    //     /* change GPIO's Pin */
-    //     if( change_flg == 0 )
-    //     {
-    //         ql_pin_set_func(QL_TEST1_PIN_GPIO0, QL_TEST1_PIN_GPIO0_FUNC_FLASH1_CLK);    // TEST1_PIN set spi_flash1_clk
-    //         ql_pin_set_func(QL_TEST1_PIN_GPIO1, QL_TEST1_PIN_GPIO1_FUNC_FLASH1_CS);     // TEST1_PIN set spi_flash1_cs
+        /* change GPIO's Pin */
+        if( change_flg == 0 )
+        {
+            ql_pin_set_func(QL_TEST1_PIN_GPIO0, QL_TEST1_PIN_GPIO0_FUNC_FLASH1_CLK);    // TEST1_PIN set spi_flash1_clk
+            ql_pin_set_func(QL_TEST1_PIN_GPIO1, QL_TEST1_PIN_GPIO1_FUNC_FLASH1_CS);     // TEST1_PIN set spi_flash1_cs
 
-    //         ql_pin_set_func(QL_TEST2_PIN_GPIO0, QL_TEST2_PIN_GPIO0_FUNC_GPIO);          // TEST2_PIN set GPIO0
-    //         ql_pin_set_func(QL_TEST2_PIN_GPIO1, QL_TEST2_PIN_GPIO1_FUNC_GPIO);          // TEST2_PIN set GPIO1
+            ql_pin_set_func(QL_TEST2_PIN_GPIO0, QL_TEST2_PIN_GPIO0_FUNC_GPIO);          // TEST2_PIN set GPIO0
+            ql_pin_set_func(QL_TEST2_PIN_GPIO1, QL_TEST2_PIN_GPIO1_FUNC_GPIO);          // TEST2_PIN set GPIO1
 
-    //         QL_GPIODEMO_LOG("GPIO0/1 pin is TEST1 -> TEST2");
-    //         change_flg = 1;
-    //     }
-    //     else
-    //     {
-    //         ql_pin_set_func(QL_TEST1_PIN_GPIO0, QL_TEST1_PIN_GPIO0_FUNC_GPIO);          // TEST1_PIN set GPIO0
-    //         ql_pin_set_func(QL_TEST1_PIN_GPIO1, QL_TEST1_PIN_GPIO1_FUNC_GPIO);          // TEST1_PIN set GPIO1
+            QL_GPIODEMO_LOG("GPIO0/1 pin is TEST1 -> TEST2");
+            change_flg = 1;
+        }
+        else
+        {
+            ql_pin_set_func(QL_TEST1_PIN_GPIO0, QL_TEST1_PIN_GPIO0_FUNC_GPIO);          // TEST1_PIN set GPIO0
+            ql_pin_set_func(QL_TEST1_PIN_GPIO1, QL_TEST1_PIN_GPIO1_FUNC_GPIO);          // TEST1_PIN set GPIO1
 
-    //         ql_pin_set_func(QL_TEST2_PIN_GPIO0, QL_TEST2_PIN_GPIO0_FUNC_LCD_SIO);       // TEST2_PIN set spi_lcd_sio
-    //         ql_pin_set_func(QL_TEST2_PIN_GPIO1, QL_TEST2_PIN_GPIO1_FUNC_LCD_SDC);       // TEST2_PIN set spi_lcd_sdc
+            ql_pin_set_func(QL_TEST2_PIN_GPIO0, QL_TEST2_PIN_GPIO0_FUNC_LCD_SIO);       // TEST2_PIN set spi_lcd_sio
+            ql_pin_set_func(QL_TEST2_PIN_GPIO1, QL_TEST2_PIN_GPIO1_FUNC_LCD_SDC);       // TEST2_PIN set spi_lcd_sdc
 
-    //         QL_GPIODEMO_LOG("GPIO0/1 pin is TEST2 -> TEST1");
-    //         change_flg = 0;
-    //     }
-    // }
+            QL_GPIODEMO_LOG("GPIO0/1 pin is TEST2 -> TEST1");
+            change_flg = 0;
+        }
+    }
 
-    //ql_rtos_task_delete(NULL);
+    ql_rtos_task_delete(NULL);
 }
 
 void ql_gpio_app_init(void)
